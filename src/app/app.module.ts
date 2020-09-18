89143b71b9b26f6812935835f5654275afbabd3c
A-Venkatesh <venkatesha@live.in>
2020-09-18 23:48:23 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateComponent } from './components/create/create.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
