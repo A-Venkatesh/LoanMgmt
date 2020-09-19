22b261daaed1c940674b12e96936a650a8b88005
A-Venkatesh <venkatesha@live.in>
2020-09-19 23:37:22 +0530

    UI Phase one completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateComponent } from './components/create/create.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +31,20 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnInit, AfterViewInit {
+  minDate: Date;
+  maxDate: Date;
+  constructor(private fb: FormBuilder) {
+    // const date = new Date();
+    // console.log('from' + date);
+    this.minDate = new Date();
+    console.log('from' + this.minDate);
+    // date.setFullYear(date.getFullYear() + 80 , 20 , 1);
+    this.maxDate = new Date();
+    console.log('to' + this.maxDate.getFullYear());
+    this.maxDate.setFullYear(this.maxDate.getFullYear() + 100);
+    console.log('to' + this.maxDate);
+    // this.tradeDate = this.minDate;
+  }
 
-  constructor() { }
+  frequency: string[] = ['Monthly', 'Quarterly', 'Half Yearly', 'Yearly'];
+  selected = 'Quarterly';
+  isExist = true;
+  // term = 'year';
+  tradeDate = new Date();
+
+  form = this.fb.group({
+
+    frequency: [this.selected, [Validators.required]],
+    loanAmount: ['', [Validators.required, Validators.min(1), Validators.pattern('^\\d+$')]],
+    intrestRate: ['', [Validators.required, Validators.min(1), Validators.pattern('[+-]?([0-9]*[.])?[0-9]+')]],
+    tradeDate: [this.tradeDate, [Validators.required]],
+    loanStartDate: [this.getStartDate(this.tradeDate)],
+    term: ['', [Validators.required, Validators.min(1), Validators.pattern('^\\d+$')]],
+    isExist: [this.isExist],
+    custID: [''],
+    matric: ['month'],
+    matureDate: [{value: '', disabled: true}],
+
+  });
+
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void{
+    this.form.controls.loanStartDate.valueChanges.subscribe(
+      () => {
+        // console.log(this.frm1 .controls.family.value)
+        this.onTermChangeEvent();
+      }
+    );
+    this.form.controls.matric.valueChanges.subscribe(
+      () => {
+        // console.log(this.frm1 .controls.family.value)
+        this.onTermChangeEvent();
+      }
+    );
+          }
+
+  getErrorMessage(filedName: string): string {
+    switch (filedName) {
+      case 'loanAmount':
+        return 'Invalid Input';
+
+      case 'intrestRate':
+        return 'Invalid Input';
+      case 'term':
+        return 'Invalid Input';
+
+      default:
+        break;
+    }
+  }
+  tradeChange(event: MatDatepickerInputEvent<Date>): void {
+    console.log('log');
+    this.form.controls.loanStartDate.setValue(this.getStartDate(event.value));
+  }
+  getStartDate(date): Date {
+    let startDate = new Date(date);
+    return new Date(startDate.setDate(startDate.getDate() + 10));
+  }
+
+  onTermChangeEvent(): void {
+    // console.log(event.target.value);
+    const no = Number(this.form.controls.term.value);
+    const startDate = new Date(this.form.controls.loanStartDate.value);
+    console.log(startDate);
+    console.log(this.form.controls.matric.value);
+    if (this.form.controls.matric.value === 'year') {
+      startDate.setFullYear(startDate.getFullYear() + no);
+      console.log(startDate);
+      this.form.controls.matureDate.setValue(startDate);
+    } else {
+      console.log(typeof(no));
+      
+      startDate.setMonth(startDate.getMonth() + no);
+      console.log(startDate);
+      this.form.controls.matureDate.setValue(startDate);
+    }
+  }
 }
